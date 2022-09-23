7730ca4587808d6f34fa2e9c3b5f5942e3c0df1e
DMMutua <davymtuerr@gmail.com>
2022-09-23 13:30:18 +0300

    Importing a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    """ Printing result of addition between two numbers"""
+    """ Printing result of addition between two numbers """
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
