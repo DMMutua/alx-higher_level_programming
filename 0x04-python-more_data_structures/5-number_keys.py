fbaa06476650691378a14cc183df6240572e1435
DMMutua <davymtuerr@gmail.com>
2022-09-28 17:31:17 +0300

    Counting no. of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    key_no = 0
+    for key in a_dictionary:
+        key_no += 1
+    return (key_no)
+# Alternative Code
+# return len(a_dictionary.keys())
