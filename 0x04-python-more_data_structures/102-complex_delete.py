079380cb4b4dc45c9a833a4d78905576de0fefc5
DMMutua <davymtuerr@gmail.com>
2022-09-28 19:00:41 +0300

    Complex Deleting by value in a dictionary

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    keys_dict = []
+
+    for k, v in a_dictionary.items():
+        if v == value:
+            keys.append(k)
+
+    for k in keys:
+        del a_dictionary[k]
+
+    return a_dictionary
