ee1aeb3086264550f1e4f4de6e78f9fa02152cc5
DMMutua <davymtuerr@gmail.com>
2022-10-11 14:58:40 +0300

    Excepting TypeError in Printing list items

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    printed_items = 0
+
+    for item in range(x):
+        try:
+            if type(my_list[item] is int and printed_items != x:
+                    print('{:d}'.format(my_list[item]), end='')
+                    printed_items += 1
+        except TypeError:
+            continue
+
+    print()
+
+    return printed_items
