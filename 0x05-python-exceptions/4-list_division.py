4cd3d535b9c62419dbbe61ed86b4fffccdba0ef9
DMMutua <davymtuerr@gmail.com>
2022-10-11 16:14:55 +0300

    excepting several errors in dividing contents of two lists: correcting pycodestyle error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,18 +9,18 @@ def list_division(my_list_1, my_list_2, list_length):
             div_result = my_list_1[item] / my_list_2[item]
 
         except TypeError:
-            print("wrong type")
+            print('wrong type')
             div_result = 0
 
         except ZeroDivisionError:
-            print("division by 0")
+            print('division by 0')
             div_result = 0
-            
+
         except IndexError:
-            print("out of range")
+            print('out of range')
             div_result = 0
 
         finally:
             resulting_list.append(div_result)
 
-    return (resulting_list)
+    return resulting_list
