bff743cee522ee0e5a980bb3e7814d32282a6029
DMMutua <davymtuerr@gmail.com>
2022-10-11 16:11:26 +0300

    excepting several errors in dividing contents of two lists: correcting return type

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-    """Divides two lists element by element.
-    Args:
-        my_list_1 (list): The first list.
-        my_list_2 (list): The second list.
-        list_length (int): The number of elements to divide.
-    Returns:
-        A new list of length list_length containing all the divisions.
-    """
+
     resulting_list = []
 
     for item in range(0, list_length):
@@ -30,4 +23,4 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             resulting_list.append(div_result)
 
-    return (new_list)
+    return (resulting_list)
