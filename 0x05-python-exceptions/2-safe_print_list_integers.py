0988ba021e6337c4a3f23f9ebbbdd6a2bc5034ee
DMMutua <davymtuerr@gmail.com>
2022-10-11 15:05:36 +0300

    Correcting Excepting TypeError handling by checking Int well

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
 
     for item in range(x):
         try:
-            if type(my_list[item] is int and printed_items != x:
+            if type(my_list[item]) is int and printed_items != x:
                     print('{:d}'.format(my_list[item]), end='')
                     printed_items += 1
         except TypeError:
