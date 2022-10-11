2ad7c23b7ae4225263caea3ab96fed2dd07a5b74
DMMutua <davymtuerr@gmail.com>
2022-10-11 16:42:06 +0300

    Prints a value in {:d}.format: Correcting comment pycodestyle

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,8 +2,7 @@
 import sys
 
 def safe_print_integer_err(value):
-    """
-    Prints an integer with "{:d}".format().
+    """Prints an integer with "{:d}".format().
     If a ValueError message is caught, a corresponding
     message is printed to standard error.
     Args:
