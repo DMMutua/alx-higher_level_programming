207fc8342faa077a3b08d6ce39fe92dfbf176d35
DMMutua <davymtuerr@gmail.com>
2022-10-11 16:40:32 +0300

    Prints a value in {:d}.format

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """
+    Prints an integer with "{:d}".format().
+    If a ValueError message is caught, a corresponding
+    message is printed to standard error.
+    Args:
+        value (int): The integer to print.
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
+    try:
+        print('{:d}'.format(value))
+        return True
+
+    except ValueError as vler:
+        sys.stderr.write("Exception: " + str(vler) + "\n")
+        return False
+
+    except TypeError as tper:
+        sys.stderr.write("Exception: " + str(tper) + "\n")
+        return False
