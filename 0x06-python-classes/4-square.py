a45c1641d7ab16f53e4203105c2e74f6734dcb3f
DMMutua <davymtuerr@gmail.com>
2022-10-12 19:34:43 +0300

    getter and setter properties to size of instance Square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Representation of a square."""
+
+    def __init__(self, size=0):
+        """Initialization of a new Square.
+        Attributes:
+            size (:obj:`int`, optional): The size of the new square
+            as private instance.
+        Raise:
+            TypeError: If `size` type is not `int`.
+            ValueError: if `size` is less than `0`.
+        """
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+
+    def area(self):
+        """Returns the Current Area of the Square in question."""
+        return self.__size ** 2
