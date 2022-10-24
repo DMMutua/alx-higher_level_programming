6ada1727a7b5dc3ea44cf4218597092ecc5e30da
DMMutua <davymtuerr@gmail.com>
2022-10-24 20:59:27 +0300

    subclass with public insance method to sort object list of superclass

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A module to prints a list in ascending order
+"""
+
+
+class MyList(list):
+    """
+    A class that customizes the list class
+    """
+
+    def print_sorted(self):
+        """
+        Printing an object 'list' in ascending order
+
+        Sorting the list and then printing the output
+        """
+
+        if issubclass(MyList, list):
+            print(sorted(self))
