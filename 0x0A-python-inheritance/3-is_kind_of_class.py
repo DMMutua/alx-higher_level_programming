8d0572180a21e687f93c205c9b4b3beb7d44fd9a
DMMutua <davymtuerr@gmail.com>
2022-10-24 21:37:16 +0300

    Checking if object is same class or inherits from a_class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a class and inherited class-checking function."""
+
+
 def is_kind_of_class(obj, a_class):
     """
     Checks if `obj` is the same class or inherit from `a_class`
@@ -11,5 +14,6 @@ def is_kind_of_class(obj, a_class):
         `True` if the object is an instance or inherit from the
         specified class; otherwise `False`
     """
-
-    return isinstance(obj, a_class)
+    if isinstance(obj, a_class):
+        return True
+    return False
