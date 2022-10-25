2b7926a7668f2bacfe6f63cbe9b46767fe45e8e1
DMMutua <davymtuerr@gmail.com>
2022-10-25 16:28:06 +0300

    Give the square and Rectangle their heritage `BaseGeometry`, and call it`s method proper

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,13 +6,14 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Class implementing blueprint of a square"""
 
-    def __init_(self, size):
+    def __init__(self, size):
         """
         Constructor of Instance of Class square
+
         Args:
             size (int): The size of the new square
         """
 
         self.integer_validator("size", size)
-        super.__init__(size, size)
+        super().__init__(size, size)
         self.__size = size
