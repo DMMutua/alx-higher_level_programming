1da960e218da353adaf1b38e2268e042dc60a03e
DMMutua <davymtuerr@gmail.com>
2022-10-25 12:07:23 +0300

    Public Instance method to validate integer

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module 7-base_geometry.
+Creates a BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """Class Containing Public Instance Methods."""
+
+    def area(self):
+        """
+        Raises: Exception with the message
+        'area() is not implemented'.
+        """
+
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """Validates An Integer Value"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{name} must be greater than 0".format(name))
