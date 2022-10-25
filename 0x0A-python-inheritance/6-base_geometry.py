21b8802e26c8d127591f0927f58e4621e9d17bf4
DMMutua <davymtuerr@gmail.com>
2022-10-25 11:32:28 +0300

    Public Instance method raising exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module: 6-base_geometry
+Creates a Class of BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Class Contains Public Instance Method."""
+
+    def area(self):
+        """
+        Raises: Exception with Message 
+        'area() is not implemented'.
+        """
+        raise Exception('area() is not implemented')
