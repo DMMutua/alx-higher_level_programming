1879906849705198d2f50d8212bbae3b4e6ac9d5
DMMutua <davymtuerr@gmail.com>
2022-10-25 16:18:50 +0300

    Give the square and Rectangle their heritage `BaseGeometry`

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle.py').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
