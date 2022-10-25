5279d4e9ec366cd2db622bdd6c53ccd673c07acc
DMMutua <davymtuerr@gmail.com>
2022-10-25 13:02:18 +0300

    print and string representation for object of Inheriting Rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Defination of a `Rectangle` class that inherits from `BaseGeometry`.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Subclass to Represent rectangle using BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Constructor to initialize new Rectangle Objects
+        Args:
+             width (`int`, private): The width of the new Rectangle.
+            height (`int`, private): The height of the new Rectangle.
+        """
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """
+        Public Instance Method
+        Returns: Area of a Rectangle Object
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Public Instance Method
+        Returns: print() and str() representations of a Rectangle Object.
+        """
+        rep = "[" + str(self.__class__.__name__) + "]"
+        rep += str(self.__width) + "/" + str(self.__height)
+        return rep
