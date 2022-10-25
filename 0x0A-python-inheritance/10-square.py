437f03c5ce23e8ee502cf30698351585f1a51cfc
DMMutua <davymtuerr@gmail.com>
2022-10-25 16:24:28 +0300

    Give the square and Rectangle their heritage `BaseGeometry`, and call it`s method proper

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -14,5 +14,5 @@ class Square(Rectangle):
         """
 
         self.integer_validator("size", size)
-        super().__init__(size, size)
+        super.__init__(size, size)
         self.__size = size
