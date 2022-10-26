31d0640929b4cfc9e58249db882b4ecd287073fb
DMMutua <davymtuerr@gmail.com>
2022-10-26 11:14:53 +0300

    A rebel subclass of `int`

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module: 11-my_int
+Defination of a class MyInt that inherits from class `int`.
+"""
+
+
+class MyInt(int):
+    """
+    Blueprint of `MyInt` objects.
+    Public Instance Methods to:
+        Invert int operator `==`.
+        Invert int operator `!=`.
+    """
+
+    def __eq__(self, value):
+        """Overrides `==` operator with `!=` behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Overriding `!=` operator with `==` behavior."""
+        return self.real == value
