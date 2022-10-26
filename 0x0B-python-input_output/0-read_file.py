f5be823012321179d37a5ae22dc1d0b060817d8b
DMMutua <davymtuerr@gmail.com>
2022-10-26 14:31:23 +0300

    Correcting the read function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 """Definantions of a text file-reading function."""
 
 
-def read_filename(filename=""):
+def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout"""
     
     with open(filename, "r", encoding="utf-8") as a_file:
