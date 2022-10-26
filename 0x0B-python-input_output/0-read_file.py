748b6eaf8e8dd8f50401b215dfa9b2cd0642ec05
DMMutua <davymtuerr@gmail.com>
2022-10-26 14:35:32 +0300

    Correcting the read function: removing trailing tab

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Definantions of a text file-reading function."""
+"""Definations of a text file-reading function."""
 
 
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout"""
-    
+    """Print the contents of a UTF8 text file to stdout."""
+
     with open(filename, "r", encoding="utf-8") as a_file:
         for line in a_file:
             print(line, end = '')
