299f4994dba889aa70a96e45ba14c789de63f315
DMMutua <lengowriter99@gmail.com>
2022-12-06 11:11:21 +0300

    computing average and inserting it in new column

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Computing the score average of all records in `second_table`
+-- Database: `hbtn_0c_0` [passed as argument to the mysql command]
+-- Result column name is `average`
+SELECT AVG(score) AS average FROM second_table;
