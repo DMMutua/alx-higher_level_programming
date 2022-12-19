66bb76465a0f903dc9ffd8558f427eb737da7a17
DMMutua <lengowriter99@gmail.com>
2022-12-19 16:04:44 +0300

    Printing members of array on separate line using console.log()

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+//Printing different text on Seperate Lines with Arrays
+
+let statements = ["C is fun","Python is cool","JavaScript is amazing"]
+for (let i = 0; i < statements.length; i++)
+{
+  console.log(statements[i]);
+}
