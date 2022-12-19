d4f73a5676ca54102093a6eb4555eb89ad75067c
DMMutua <lengowriter99@gmail.com>
2022-12-19 12:34:21 +0300

    3.Correcting semicolon + 4.formatting Args the Way I want using process.argv

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,4 +4,4 @@
 
 const first_script_argument = process.argv[2];
 
-console.log(typeof first_script_argument === 'undefined' ? 'No argument' : first_script_argument)
+console.log(typeof first_script_argument === 'undefined' ? 'No argument' : first_script_argument);
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+// Script for Printing Two Args passed to Itself in the format;
+// `arg1` is `arg2`
+
+console.log(process.argv[2], "is", process.argv[3]);
