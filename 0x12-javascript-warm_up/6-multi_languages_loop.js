a529314d5c6141788dcea348bfda3bd67fccf33c
DMMutua <lengowriter99@gmail.com>
2022-12-19 16:06:29 +0300

    Correcting the array syntax.

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 
 //Printing different text on Seperate Lines with Arrays
 
-let statements = ["C is fun","Python is cool","JavaScript is amazing"]
+let statements = ["C is fun", "Python is cool", "JavaScript is amazing"]
 for (let i = 0; i < statements.length; i++)
 {
   console.log(statements[i]);
