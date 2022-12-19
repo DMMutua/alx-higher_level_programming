7762be6dd0d3c144d8cab003146b99a3e541ca86
DMMutua <lengowriter99@gmail.com>
2022-12-19 18:30:32 +0300

    Printing A representation of a Square.

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+//A script that prints a square with the 'X' Letter
+
+const square_size = Math.floor(Number(process.argv[2]));
+if !(isNaN(square_size)
+{
+  for (i = 0; i <= square_size; i++)
+  {
+    for (1 = 0; i <= square_size; i++)
+    {
+      console.log('X');
+    }
+  }
+}
