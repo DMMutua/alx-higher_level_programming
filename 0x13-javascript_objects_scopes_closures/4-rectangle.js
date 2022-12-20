7645cb8dbab193e75f2a357c2e685e61c2fe9957
DMMutua <lengowriter99@gmail.com>
2022-12-20 16:28:22 +0300

    Rotating and Doubling The Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+// Checks for Rectangle parameters
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rows = '';
+      for (let j = 0; j < this.width; j++) {
+        rows += 'X';
+    }
+      console.log(row);
+  }
+
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
+
