52ff94a615224ea29a036293c4d6051a954f85c2
DMMutua <lengowriter99@gmail.com>
2022-12-20 16:12:12 +0300

    Printing a Rectangle of Exes

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+// Checks for Rectangle parameters
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rows = '';
+      for (let j = 0; j < this.width; j++) {
+	rows += 'X';
+    }
+      console.log(row);
+  }
+};
