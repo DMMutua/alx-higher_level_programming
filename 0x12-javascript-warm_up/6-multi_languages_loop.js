e7357038316bde8866d6edc98299e270b53d16ee
DMMutua <lengowriter99@gmail.com>
2022-12-21 11:34:26 +0300

    We Can Also Print in An Array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const array_of_sentences = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < array_of_sentences.length; ++i) {
+  console.log(array_of_sentences[i]);
+}
