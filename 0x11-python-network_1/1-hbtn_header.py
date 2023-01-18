a23040641bb3d94045248dd6c7aeda422e9b9033
DMMutua <lengowriter99@gmail.com>
2023-01-18 11:49:50 +0300

    removing the __main__ guard

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,5 @@ from urllib.request import urlopen
 from sys import argv
 
 
-if __name__ == '__main__':
-    with urlopen(argv[1]) as response:
-        print(response.getheader('X-Request-Id'))
+with urlopen(argv[1]) as response:
+    print(response.getheader('X-Request-Id'))
