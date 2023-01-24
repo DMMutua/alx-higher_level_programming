185f299dae433bd0248ca9435be8d3bc703aa984
DMMutua <lengowriter99@gmail.com>
2023-01-24 14:44:50 +0300

    changing request contents

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email: sys.argv[2]"}
+    value = {"Your email is: sys.argv[2]"}
     data = urllib.parse.urlencode(value).encode("ascii")
 
     request = urllib.request.Request(url, data)
